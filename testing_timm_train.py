dcf6ff8d6481f89a17866d6a504ef594a804c371
Youssef Farag <youssef.m.farag@gmail.com>
2023-04-20 14:44:32 +0200

    ca

--- testing_timm_train.py
+++ testing_timm_train.py
@@ -2,14 +2,11 @@ import yaml
 import argparse
 import numpy as np
 import torch
-from collections import defaultdict
-from torchvision import transforms
 from PIL import Image
 from timm.models import create_model
 import metaformer_baselines
+from timm import utils
 from timm.data import create_dataset, create_loader, resolve_data_config, transforms_factory
-from timm.data.loader import _worker_init, PrefetchLoader
-from functools import partial
 from itertools import repeat
 
 def fast_collate(batch):
@@ -78,20 +75,6 @@ group.add_argument('--class-map', default='', type=str, metavar='FILENAME',
 group = parser.add_argument_group('Model parameters')
 group.add_argument('--model', default='resnet50', type=str, metavar='MODEL',
                     help='Name of model to train (default: "resnet50"')
-group.add_argument('--pretrained', action='store_true', default=False,
-                    help='Start with pretrained version of specified network (if avail)')
-group.add_argument('--initial-checkpoint', default='', type=str, metavar='PATH',
-                    help='Initialize model from this checkpoint (default: none)')
-group.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='Resume full model and optimizer state from checkpoint (default: none)')
-group.add_argument('--no-resume-opt', action='store_true', default=False,
-                    help='prevent resume of optimizer state when resuming model')
-group.add_argument('--num-classes', type=int, default=None, metavar='N',
-                    help='number of label classes (Model default if None)')
-group.add_argument('--gp', default=None, type=str, metavar='POOL',
-                    help='Global pool type, one of (fast, avg, max, avgmax, avgmaxc). Model default if None.')
-group.add_argument('--img-size', type=int, default=None, metavar='N',
-                    help='Image patch size (default: None => model default)')
 group.add_argument('--input-size', default=None, nargs=3, type=int,
                     metavar='N N N', help='Input all image dimensions (d h w, e.g. --input-size 3 224 224), uses model default if empty')
 group.add_argument('--crop-pct', default=None, type=float,
@@ -104,15 +87,6 @@ group.add_argument('--interpolation', default='', type=str, metavar='NAME',
                     help='Image resize interpolation type (overrides model)')
 group.add_argument('-b', '--batch-size', type=int, default=128, metavar='N',
                     help='Input batch size for training (default: 128)')
-
-# Learning rate schedule parameters
-group = parser.add_argument_group('Learning rate schedule parameters')
-group.add_argument('--sched', default='cosine', type=str, metavar='SCHEDULER',
-                    help='LR scheduler (default: "cosine"')
-group.add_argument('--lr', type=float, default=0.05, metavar='LR',
-                    help='learning rate (default: 0.05)')
-group.add_argument('--epochs', type=int, default=300, metavar='N',
-                    help='number of epochs to train (default: 300)')
 
 # Augmentation & regularization parameters
 group = parser.add_argument_group('Augmentation and regularization parameters')
@@ -130,12 +104,6 @@ group.add_argument('--color-jitter', type=float, default=0.4, metavar='PCT',
                     help='Color jitter factor (default: 0.4)')
 group.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
                     help='Use AutoAugment policy. "v0" or "original". (default: rand-m9-mstd0.5-inc1)'),
-group.add_argument('--jsd-loss', action='store_true', default=False,
-                    help='Enable Jensen-Shannon Divergence + CE loss. Use with `--aug-splits`.')
-group.add_argument('--bce-loss', action='store_true', default=False,
-                    help='Enable BCE loss w/ Mixup/CutMix use.')
-group.add_argument('--bce-target-thresh', type=float, default=None,
-                    help='Threshold for binarizing softened BCE targets (default: None, disabled)')
 group.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
                     help='Random erase prob (default: 0.25)')
 group.add_argument('--train-interpolation', type=str, default='bilinear',
@@ -147,10 +115,20 @@ group.add_argument('--seed', type=int, default=42, metavar='S',
                     help='random seed (default: 42)')
 group.add_argument('-j', '--workers', type=int, default=1, metavar='N',
                     help='how many training processes to use (default: 8)')
-group.add_argument('--output', default='', type=str, metavar='PATH',
-                    help='path to output folder (default: none, current dir)')
-group.add_argument('--eval-metric', default='f1_score', type=str, metavar='EVAL_METRIC',
-                    help='Best metric (default: "f1_score"')
+group.add_argument('--no-prefetcher', action='store_true', default=False,
+                    help='disable fast prefetcher')
+group.add_argument('--remode', type=str, default='pixel',
+                    help='Random erase mode (default: "pixel")')
+group.add_argument('--recount', type=int, default=1,
+                    help='Random erase count (default: 1)')
+group.add_argument('--resplit', action='store_true', default=False,
+                    help='Do not random erase first (clean) augmentation split')
+group.add_argument('--pin-mem', action='store_true', default=False,
+                   help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+group.add_argument('--use-multi-epochs-loader', action='store_true', default=False,
+                    help='use the multi-epochs-loader to save time at the beginning of every epoch')
+group.add_argument('--worker-seeding', type=str, default='all',
+                    help='worker seed mode (default: all)')
 
 def _parse_args():
     # Do we have a config file to parse?
@@ -171,49 +149,52 @@ def _parse_args():
 
 def main():
     args, args_text = _parse_args()
+    args.rank = 0
+    utils.random_seed(args.seed, args.rank)
     use_prefetcher = True
-    args.data_dir = '../ba_project/datasets/retest'
-    modargs = {'model_name': 'convformer_s18_384', 'pretrained': False, 'num_classes': None,
-               'drop_rate': 0.0, 'drop_connect_rate': None, 'drop_path_rate': 0.3,
-               'drop_block_rate': None, 'global_pool': None, 'bn_momentum': None,
-               'bn_eps': None, 'scriptable': False,
-               'checkpoint_path': './convformer_s18_384.pth',
-               'head_dropout': 0.4}
+    args.data_dir = '/home/yous/Desktop/cerrion/datasets/retest'
+    modargs = {'model_name': 'convformer_s18_384', 'pretrained': True, 'num_classes': 3,
+               'checkpoint_path': './convformer_s18_384.pth'}
     a = create_model(**modargs)
     data_config = resolve_data_config(vars(args), model=a, verbose=True)
+
+    train_interpolation = args.train_interpolation
+    if args.no_aug or not train_interpolation:
+        train_interpolation = data_config['interpolation']
+
     dataset_train = create_dataset(
         args.dataset, root=args.data_dir, split=args.train_split, is_training=True,
         class_map=args.class_map, download=False, batch_size=1, repeats=0)
     impath, im_label = dataset_train.parser.samples[1]
     im = Image.open(impath).convert('RGB')
-
+    num_aug_splits = 0
+    args.prefetcher = not args.no_prefetcher
     loader_train = create_loader(
         dataset_train,
         input_size=data_config['input_size'],
-        batch_size=1,
+        batch_size=args.batch_size,
         is_training=True,
-        use_prefetcher=use_prefetcher,
-        no_aug=True,
+        use_prefetcher=args.prefetcher,
+        no_aug=args.no_aug,
         re_prob=args.reprob,
-        re_mode='pixel',
-        re_count=1,
-        re_split=False,
+        re_mode=args.remode,
+        re_count=args.recount,
+        re_split=args.resplit,
         scale=args.scale,
         ratio=args.ratio,
         hflip=args.hflip,
         vflip=args.vflip,
         color_jitter=args.color_jitter,
         auto_augment=args.aa,
-        num_aug_splits=0,
-        interpolation='random',
+        num_aug_splits=num_aug_splits,
+        interpolation=train_interpolation,
         mean=data_config['mean'],
         std=data_config['std'],
         num_workers=args.workers,
-        collate_fn=fast_collate,
-        pin_memory=False,
-        use_multi_epochs_loader=False,
-        worker_seeding='all',
-        tf_preprocessing=False
+        collate_fn=None,
+        pin_memory=args.pin_mem,
+        use_multi_epochs_loader=args.use_multi_epochs_loader,
+        worker_seeding=args.worker_seeding,
     )
     # loader_train.dataset.transform = transf_torch
     # trans_tiff = loader_train.dataset.transform
@@ -240,7 +221,6 @@ def main():
         separate=False)
     print(trans_tiff)
     print(loader_train.dataset.transform)
-    exit()
     im_tiff = trans_tiff(im)
     mean = expand_to_chs(data_config['mean'], 3)
     std = expand_to_chs(data_config['std'], 3)
