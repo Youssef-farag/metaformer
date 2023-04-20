ac4a84f4ec49d1473cfeab4f955d8c9b307a0b4b
Youssef Farag <youssef.m.farag@gmail.com>
2023-04-20 15:10:51 +0200

    asjdfla

---
+++ val_timm.py
@@ -0,0 +1,214 @@
+import yaml
+import argparse
+import numpy as np
+import torch
+from PIL import Image
+from timm.models import create_model
+import metaformer_baselines
+from timm import utils
+from timm.data import create_dataset, create_loader, resolve_data_config, transforms_factory
+from itertools import repeat
+
+def fast_collate(batch):
+    """ A fast collation function optimized for uint8 images (np array or torch) and int64 targets (labels)"""
+    assert isinstance(batch[0], tuple)
+    batch_size = len(batch)
+    if isinstance(batch[0][0], tuple):
+        # This branch 'deinterleaves' and flattens tuples of input tensors into one tensor ordered by position
+        # such that all tuple of position n will end up in a torch.split(tensor, batch_size) in nth position
+        inner_tuple_size = len(batch[0][0])
+        flattened_batch_size = batch_size * inner_tuple_size
+        targets = torch.zeros(flattened_batch_size, dtype=torch.int64)
+        tensor = torch.zeros((flattened_batch_size, *batch[0][0][0].shape), dtype=torch.uint8)
+        for i in range(batch_size):
+            assert len(batch[i][0]) == inner_tuple_size  # all input tensor tuples must be same length
+            for j in range(inner_tuple_size):
+                targets[i + j * batch_size] = batch[i][1]
+                tensor[i + j * batch_size] += torch.from_numpy(batch[i][0][j])
+        return tensor, targets
+    elif isinstance(batch[0][0], np.ndarray):
+        targets = torch.tensor([b[1] for b in batch], dtype=torch.int64)
+        assert len(targets) == batch_size
+        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=torch.uint8)
+        for i in range(batch_size):
+            tensor[i] += torch.from_numpy(batch[i][0])
+        return tensor, targets
+    elif isinstance(batch[0][0], torch.Tensor):
+        targets = torch.tensor([b[1] for b in batch], dtype=torch.int64)
+        assert len(targets) == batch_size
+        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=torch.uint8)
+        for i in range(batch_size):
+            tensor[i].copy_(batch[i][0])
+        return tensor, targets
+    else:
+        assert False
+
+def expand_to_chs(x, n):
+    if not isinstance(x, (tuple, list)):
+        x = tuple(repeat(x, n))
+    elif len(x) == 1:
+        x = x * n
+    else:
+        assert len(x) == n, 'normalization stats must match image channels'
+    return x
+
+config_parser = parser = argparse.ArgumentParser(description='Training Config', add_help=False)
+parser.add_argument('-c', '--config', default='', type=str, metavar='FILE',
+                    help='YAML config file specifying default arguments')
+
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+
+# Dataset parameters
+group = parser.add_argument_group('Dataset parameters')
+# Keep this argument outside of the dataset group because it is positional.
+# parser.add_argument('data_dir', metavar='DIR', help='path to dataset',
+#                     default='/home/yous/Desktop/cerrion/datasets/retest')
+group.add_argument('--dataset', '-d', metavar='NAME', default='ImageFolder',
+                    help='dataset type (default: ImageFolder/ImageTar if empty)')
+parser.add_argument('--split', metavar='NAME', default='validation',
+                    help='dataset split (default: validation)')
+group.add_argument('--class-map', default='', type=str, metavar='FILENAME',
+                    help='path to class to idx mapping file (default: "")')
+
+# Model parameters
+group = parser.add_argument_group('Model parameters')
+group.add_argument('--model', default='resnet50', type=str, metavar='MODEL',
+                    help='Name of model to train (default: "resnet50"')
+group.add_argument('--input-size', default=None, nargs=3, type=int,
+                    metavar='N N N', help='Input all image dimensions (d h w, e.g. --input-size 3 224 224), uses model default if empty')
+group.add_argument('--crop-pct', default=None, type=float,
+                    metavar='N', help='Input image center crop percent (for validation only)')
+group.add_argument('--mean', type=float, nargs='+', default=None, metavar='MEAN',
+                    help='Override mean pixel value of dataset')
+group.add_argument('--std', type=float, nargs='+', default=None, metavar='STD',
+                    help='Override std deviation of dataset')
+group.add_argument('--interpolation', default='', type=str, metavar='NAME',
+                    help='Image resize interpolation type (overrides model)')
+group.add_argument('-b', '--batch-size', type=int, default=1, metavar='N',
+                    help='Input batch size for training (default: 128)')
+
+# Augmentation & regularization parameters
+group = parser.add_argument_group('Augmentation and regularization parameters')
+group.add_argument('--no-aug', action='store_true', default=True,
+                    help='Disable all training augmentation, override other train aug args')
+group.add_argument('--scale', type=float, nargs='+', default=[0.08, 1.0], metavar='PCT',
+                    help='Random resize scale (default: 0.08 1.0)')
+group.add_argument('--ratio', type=float, nargs='+', default=[3./4., 4./3.], metavar='RATIO',
+                    help='Random resize aspect ratio (default: 0.75 1.33)')
+group.add_argument('--hflip', type=float, default=0.5,
+                    help='Horizontal flip training aug probability')
+group.add_argument('--vflip', type=float, default=0.,
+                    help='Vertical flip training aug probability')
+group.add_argument('--color-jitter', type=float, default=0.4, metavar='PCT',
+                    help='Color jitter factor (default: 0.4)')
+group.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
+                    help='Use AutoAugment policy. "v0" or "original". (default: rand-m9-mstd0.5-inc1)'),
+group.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
+                    help='Random erase prob (default: 0.25)')
+group.add_argument('--train-interpolation', type=str, default='bilinear',
+                    help='Training interpolation (random, bilinear, bicubic default: "random")')
+
+# Misc
+group = parser.add_argument_group('Miscellaneous parameters')
+group.add_argument('--seed', type=int, default=42, metavar='S',
+                    help='random seed (default: 42)')
+group.add_argument('-j', '--workers', type=int, default=1, metavar='N',
+                    help='how many training processes to use (default: 8)')
+group.add_argument('--no-prefetcher', action='store_true', default=False,
+                    help='disable fast prefetcher')
+group.add_argument('--remode', type=str, default='pixel',
+                    help='Random erase mode (default: "pixel")')
+group.add_argument('--recount', type=int, default=1,
+                    help='Random erase count (default: 1)')
+group.add_argument('--resplit', action='store_true', default=False,
+                    help='Do not random erase first (clean) augmentation split')
+group.add_argument('--pin-mem', action='store_true', default=False,
+                   help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
+group.add_argument('--use-multi-epochs-loader', action='store_true', default=False,
+                    help='use the multi-epochs-loader to save time at the beginning of every epoch')
+group.add_argument('--worker-seeding', type=str, default='all',
+                    help='worker seed mode (default: all)')
+
+parser.add_argument('--class-map', default='', type=str, metavar='FILENAME',
+                    help='path to class to idx mapping file (default: "")')
+parser.add_argument('--tf-preprocessing', action='store_true', default=False,
+                    help='Use Tensorflow preprocessing pipeline (require CPU TF installed')
+parser.add_argument('--dataset-download', action='store_true', default=False,
+                    help='Allow download of dataset for torch/ and tfds/ datasets that support it.')
+
+def _parse_args():
+    # Do we have a config file to parse?
+    args_config, remaining = config_parser.parse_known_args()
+    if args_config.config:
+        with open(args_config.config, 'r') as f:
+            cfg = yaml.safe_load(f)
+            parser.set_defaults(**cfg)
+
+    # The main arg parser parses the rest of the args, the usual
+    # defaults will have been overridden if config file specified.
+    args = parser.parse_args(remaining)
+
+    # Cache the args as a text string to save them in the output dir later
+    args_text = yaml.safe_dump(args.__dict__, default_flow_style=False)
+    return args, args_text
+
+
+def main():
+    args, args_text = _parse_args()
+    args.rank = 0
+    utils.random_seed(args.seed, args.rank)
+    args.data_dir = '../ba_project/datasets/retest'
+    modargs = {'model_name': 'convformer_s18_384', 'checkpoint_path': './convformer_s18_384.pth'}
+    a = create_model(**modargs)
+    data_config = resolve_data_config(vars(args), model=a, verbose=True)
+
+    dataset = create_dataset(
+        root=args.data_dir, name=args.dataset, split=args.split,
+        download=args.dataset_download, load_bytes=args.tf_preprocessing, class_map=args.class_map)
+
+    impath, im_label = dataset.parser.samples[1]
+    im = Image.open(impath).convert('RGB')
+    args.prefetcher = not args.no_prefetcher
+    img_size = (384, 384)
+    crop_pct = data_config['crop_pct']
+    loader = create_loader(
+        dataset,
+        input_size=data_config['input_size'],
+        batch_size=args.batch_size,
+        use_prefetcher=args.prefetcher,
+        interpolation=data_config['interpolation'],
+        mean=data_config['mean'],
+        std=data_config['std'],
+        num_workers=args.workers,
+        crop_pct=crop_pct,
+        pin_memory=args.pin_mem,
+        tf_preprocessing=args.tf_preprocessing)
+
+    trans_tiff = transforms_factory.transforms_imagenet_eval(
+        img_size,
+        interpolation=data_config['interpolation'],
+        use_prefetcher=args.prefetcher,
+        mean=data_config['mean'],
+        std=data_config['std'],
+        crop_pct=crop_pct)
+
+    print(trans_tiff)
+    print(loader.dataset.transform)
+    im_tiff = trans_tiff(im)
+    mean = expand_to_chs(data_config['mean'], 3)
+    std = expand_to_chs(data_config['std'], 3)
+    normalization_shape = (1, 3, 1, 1)
+    mean = torch.tensor([x * 255 for x in mean]).view(normalization_shape)
+    std = torch.tensor([x * 255 for x in std]).view(normalization_shape)
+    im_tiff, im_label = fast_collate([(im_tiff, im_label)])
+    im_tiff = im_tiff.float().sub_(mean).div_(std)
+
+    samples = []
+    for x in loader:
+        samples.append(x[0])
+
+    print(torch.equal(im_tiff.cpu(), samples[0].cpu()))
+
+if __name__ == '__main__':
+    main()
